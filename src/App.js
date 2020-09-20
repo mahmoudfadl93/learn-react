d91e907fe927fd8579cd78d6f2412aa97300f1b5
mahmoud.fadl.FLAIRSTECH <mfadl4201@gmail.com>
2020-09-20 06:41:27 +0200

    Project 1 Start Github Finder React Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+//import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
+    //8. Intro To JSX & Fragments
+    // return (
+    //   <Fragment>
+    //     <h1>Hello From React</h1>
+    //   </Fragment>
+    // );
+    // return (
+    //   <div className="App">
+    //     <h1>Hello From {name}</h1>
+    //   </div>
+    // );
+
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hello From React")
+    // );
+
+    // 9. Expressions & Conditionals In JSX
+    const name = "mahmoud";
+    const loading = false;
+    const showName = false;
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        {loading ? (
+          <h4>loading.....</h4>
+        ) : (
+          <h1>Hello From {showName && name}</h1>
+        )}
       </div>
     );
   }
