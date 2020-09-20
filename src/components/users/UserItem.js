a26f1e83b407ec892fc864cba70d62b3fb4b24fb
mahmoud.fadl.FLAIRSTECH <mfadl4201@gmail.com>
2020-09-20 15:58:59 +0200

    13. Stateless Functional Components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,50 +1,29 @@
-import React, { Component } from "react";
-
-class UserItem extends Component {
-  //------- frist way
-
-  //   constructor() {
-  //     super();
-  //     this.state = {
-  //       id: "id",
-  //       login: "mojombo",
-  //       avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //       html_url: "https://github.com/mojombo",
-  //     };
-  //   }
-
-  //------ second way
-
-  //   state = {
-  //     id: "id",
-  //     login: "mojombo",
-  //     avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //     html_url: "https://github.com/mojombo",
-  //   };
-
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-          {/* <Link to={`/user/${this.state.login}`} className='btn btn-dark btn-sm my-1'>
+import React from "react";
+import PropTypes from "prop-types";
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
+        {/* <Link to={`/user/${this.state.login}`} className='btn btn-dark btn-sm my-1'>
           More
         </Link> */}
-        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
