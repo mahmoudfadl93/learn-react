932746af903583f83e6961990914e0f4ad5ed56f
mahmoud.fadl.FLAIRSTECH <mfadl4201@gmail.com>
2020-09-21 16:16:33 +0200

    Passing Props Up & Search

--- src/App.js
+++ src/App.js
@@ -12,21 +12,23 @@ class App extends Component {
     users: [],
     loading: false,
   };
-  async componentDidMount() {
+
+  searchUsers = async (text) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    console.log(res.data);
-    this.setState({ loading: false, users: res.data });
-  }
+    console.log(res.data.items);
+    this.setState({ loading: false, users: res.data.items });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
-      
+
         <div className="container">
-        <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
