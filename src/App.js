35a04a0bf609f846fc8d89512f2c30cab2548705
mahmoud.fadl.FLAIRSTECH <mfadl4201@gmail.com>
2020-09-21 16:32:56 +0200

    Clear Users From State

--- src/App.js
+++ src/App.js
@@ -14,23 +14,40 @@ class App extends Component {
   };
 
   searchUsers = async (text) => {
-    this.setState({ loading: true });
+    this.setState({
+      loading: true,
+    });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     console.log(res.data.items);
-    this.setState({ loading: false, users: res.data.items });
+    this.setState({
+      loading: false,
+      users: res.data.items,
+    });
+  };
+
+  clearUsers = async () => {
+    this.setState({
+      users: [],
+      loading: false,
+    });
   };
 
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
-
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />{" "}
+          <Users loading={loading} users={users} />{" "}
+        </div>{" "}
       </div>
     );
   }
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,21 +6,22 @@ export class Search extends Component {
   };
 
   // static PropTypes = {
-  //   searchUsers: PropTypes.func.isRequired
+  //   searchUsers: PropTypes.func.isRequired,
+  //   clearUsers: PropTypes.func.isRequired,
+  //   showClear: PropTypes.bool.isRequired,
   // };
   onChange = (e) => {
-
     this.setState({ [e.target.name]: e.target.value });
-
   };
   onSubmit = (e) => {
     e.preventDefault();
     console.log(this.state.text);
     this.props.searchUsers(this.state.text);
-    this.setState({  text: " " });
-  }
+    this.setState({ text: " " });
+  };
 
   render() {
+    const { clearUsers, showClear } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -38,6 +39,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
